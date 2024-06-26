8d3a6fee24f1724ff46628b4ab09377a6c3914f7
Daniil Tihomirov <daniltihomirov946@gmail.com>
2024-06-26 23:36:08 +0300

    edit config

--- src/config_log.py
+++ src/config_log.py
@@ -1,11 +1,12 @@
 import logging
 from typing import Any
+import os.path
 
 
 def setting_log(name: Any) -> logging.Logger:
     """настройки логера"""
     logger = logging.getLogger(name)
-    file_handler = logging.FileHandler(f"{name}.log", "w", encoding="utf-8")
+    file_handler = logging.FileHandler(f"{os.path.join('..', 'log', name)}.log", "w", encoding="utf-8")
     file_formatter = logging.Formatter("%(asctime)s %(module)s %(funcName)s %(levelname)s: %(message)s")
     file_handler.setFormatter(file_formatter)
     logger.addHandler(file_handler)
